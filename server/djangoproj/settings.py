7689bb39c3c021c5e4f8a7f073cf70b77138220d
CardBoardBoks12 <120328460+CardBoardBoks12@users.noreply.github.com>
2024-12-14 16:05:56 -0500

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://smills0104-8000.theiadockernext-0-' \
+ALLOWED_HOSTS = ['localhost', 'https://smills0104-8000.theiadockernext-0-'
                  'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://smills0104-8000.theiadockernext-0-labs-' \
+CSRF_TRUSTED_ORIGINS = ['https://smills0104-8000.theiadockernext-0-labs-'
                         'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
@@ -96,7 +96,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.' \
+        'django.contrib.auth.password_validation.'
         'UserAttributeSimilarityValidator',
     },
     {
