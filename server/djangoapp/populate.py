51d7bfbd5cf1a87fb0a1da5d046d91b3d5d44c91
CardBoardBoks12 <120328460+CardBoardBoks12@users.noreply.github.com>
2024-12-14 15:03:03 -0500

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -13,7 +13,7 @@ def initiate():
     car_make_instances = []
     for data in car_make_data:
         car_make_instances.append(CarMake.objects.create
-                                  (name=data['name'], 
+                                  (name=data['name'],
                                    description=data['description']))
 
     # Create CarModel instances with the corresponding CarMake instances
@@ -48,8 +48,8 @@ def initiate():
        "car_make": car_make_instances[4]},
       {"name": "Kluger", "type": "SUV", "year": 2023,
        "car_make": car_make_instances[4]},
-        # Add more CarModel instances as needed
+       # Add more CarModel instances as needed
     ]
 
     for data in car_model_data:
-            CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
+        CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
